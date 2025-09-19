2d157763d9079b5fbe02575ddf5fd6a99a71175d
KB1537 <Kylebenjamin437@gmail.com>
2025-09-19 16:02:58 +0100

    define data collection and moving avg function

--- app.py
+++ app.py
@@ -22,3 +22,21 @@ except Exception:
 
 #app helpers
 st.cache_data(ttl=3600) #cache downloaded data for one hour 
+
+#downloads hsitorical data(Open,high,low,close,volume) and returns clean data with datetime index
+def fetch_ticker_data(ticker: str, start:str, end:str )-> pd.DataFrame:
+    df = yf.download (ticker, start=start, end=end, progress=False, auto_adjust=True)
+    if df.empty:
+        return pd.DataFrame()
+    df.index=pd.to_datetime(df.index)
+    df=df[['open','high','low','close','volumn']]
+    df=df.sort_index()
+    return df
+
+
+def moving_avg(df: pd.DataFrame, windows=[50,200]):
+    for w in windows:
+        df[f"MA_{w}"]=df['close'].rolling(window=w).mean()
+    return df
+    
+
